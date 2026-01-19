c4317dc28ae567dc4b174783bba5134c3082f699
Kabanya <evonirazh@mail.ru>
2026-01-19 03:09:48 +0300

    in new places queries

---
+++ migrations/queries/all-query.sql
@@ -0,0 +1,6 @@
+
+
+SELECT * FROM customers   LIMIT 5;
+SELECT * FROM couriers    LIMIT 5;
+SELECT * FROM orders      LIMIT 5;
+SELECT * FROM restaurants LIMIT 5;
---
+++ migrations/queries/all_tables.sql
@@ -0,0 +1,10 @@
+SELECT table_name
+FROM information_schema.tables
+WHERE table_schema = 'public'
+AND table_name NOT IN (
+    'goose_restaurant_version',
+    'goose_courier_version',
+    'goose_orders_version',
+    'goose_customers_version',
+    'goose_customer_version'
+);
